c6e6d697affddf6f0f2011dd23dca1b664a3ccf2
Alle4n <allennajjuma96@gmail.com>
2025-09-28 13:36:17 +0300

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Perform basic arithmetic operations: add, subtract, multiply, divide.
     Handles division by zero by returning an error message.
